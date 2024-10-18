0a4322edfad65b2147c1b372bfdb3fd781628018
Namratha Adidela <145983546+NamrathaAdidela@users.noreply.github.com>
2024-10-18 10:58:54 +0100

    Update carClass.java

--- src/Inheritence/carClass.java
+++ src/Inheritence/carClass.java
@@ -9,5 +9,7 @@ public class carClass {
 	public final void test1(String str)
 	{
 		System.out.println("Final Method1");
+		System.out.println("Updating001");
+		System.out.println("Changes made for GIT 2nd commit");
 	}
 }
